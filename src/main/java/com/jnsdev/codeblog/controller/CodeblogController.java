cb667353d408a41d497c14d189bd5573d5b3a8dd
jairosousa <jaironsousa@gmail.com>
2020-03-13 16:30:08 -0300

    Inserindo mensagem de validação no Form e SummerNote para formatação

--- src/main/java/com/jnsdev/codeblog/controller/CodeblogController.java
+++ src/main/java/com/jnsdev/codeblog/controller/CodeblogController.java
@@ -45,6 +45,7 @@ public class CodeblogController {
     @RequestMapping(value = "/newpost", method = RequestMethod.POST)
     public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
         if (result.hasErrors()) {
+            attributes.addFlashAttribute("messagem", "Verifique se os campos obrigatórios foram preenchidos");
             return "redirect:/newpost";
         }
         post.setData(LocalDate.now());
